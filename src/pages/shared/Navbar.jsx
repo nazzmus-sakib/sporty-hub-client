4cc8c3c49ac67fa75339c303d4ffdeb5614a8737
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-06-12 15:23:16 +0600

    some design change on navbar

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -44,56 +44,57 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100 z-10 shadow-sm  border-b-2 lg:px-32 lg:py-5 px-2 py-3 sticky top-0 ">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className=" bg-base-100 z-10 shadow-sm border-b-2 ">
+      <div className="max-w-7xl mx-auto navbar lg:px-0 lg:py-5 px-2 py-3 sticky top-0 ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {navItems}
-          </ul>
+              {navItems}
+            </ul>
+          </div>
+          <Link to="/">
+            <h2 className="primary-text text-xl lg:text-3xl font-bold  uppercase">
+              sporty hub
+            </h2>
+          </Link>
         </div>
-        <Link to="/">
-          <h2 className="primary-text text-xl lg:text-3xl font-bold lg:ml-4 ml-2 uppercase">
-            sporty hub
-          </h2>
-        </Link>
-      </div>
-      <div className="navbar-end hidden lg:flex items-center">
-        <ul className="menu menu-horizontal px-1 text-base">{navItems}</ul>
-        <div className="">
-          {user?.photoURL ? (
-            <div
-              className="tooltip tooltip-left tooltip-warning mr-5"
-              data-tip={user?.displayName}
-            >
-              <div className="avatar online ">
-                <div className="w-12 rounded-full ">
-                  <img src={user?.photoURL} />
+        <div className="navbar-end hidden lg:flex items-center">
+          <ul className="menu menu-horizontal px-1 text-base">{navItems}</ul>
+          <div className="">
+            {user?.photoURL ? (
+              <div
+                className="tooltip tooltip-left tooltip-warning mr-5"
+                data-tip={user?.displayName}
+              >
+                <div className="avatar online ">
+                  <div className="w-12 rounded-full ">
+                    <img src={user?.photoURL} />
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <FaRegUserCircle className=" text-4xl"></FaRegUserCircle>
-            // <FaUserCircle className="lg:text-5xl text-4xl"></FaUserCircle>
-          )}
+            ) : (
+              <FaRegUserCircle className=" text-4xl"></FaRegUserCircle>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import Login from "../pages/login/Login";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
